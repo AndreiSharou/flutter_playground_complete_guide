5479b97b6a3f234232321dc42e7362dd725eb937
AndreiSharou <Microlab_22>
2021-01-05 11:49:34 +0300

    end with quiz

--- lib/result.dart
+++ lib/result.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
-
   final int resultScore;
+  final Function resetHandler;
 
-  Result(this.resultScore);
+  Result(this.resultScore, this.resetHandler);
 
   String get resultPhrase {
-    var resultText;
+    String resultText;
     if (resultScore <= 8) {
       resultText = 'You are awesome and innocent!';
     } else if (resultScore <= 12) {
@@ -23,8 +23,16 @@ class Result extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(resultPhrase,
-          style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold)),
+      child: Column(
+        children: <Widget>[
+          Text(
+            resultPhrase,
+            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          FlatButton(onPressed: resetHandler, child: Text('Restart Quiz!'), textColor: Colors.blue,)
+        ],
+      ),
     );
   }
 }
